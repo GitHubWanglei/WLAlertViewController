60337567b297e1cdf1eb05d372ba2fbf4d01d183
GitHubWanglei <963256255@qq.com>
2016-01-04 14:17:25 +0800

    .
    .

--- example/alertView/ViewController.m
+++ example/alertView/ViewController.m
@@ -25,8 +25,19 @@
 
 - (IBAction)btn:(id)sender {
     
-    WLAlertViewController *alertv=[WLAlertViewController alertWithTitle:@"提示消息" message:@"登陆失败" delegate:self cancelButtonTitle:@"确定" preferredStyle:WLAlertControllerStyleAlert];
+    //初始化
+    WLAlertViewController *alertv=[WLAlertViewController alertWithTitle:@"提示消息"
+                                                                message:@"登陆失败"
+                                                               delegate:self
+                                                      cancelButtonTitle:@"确定"
+                                                         preferredStyle:WLAlertControllerStyleAlert];
+    //添加按钮
     [alertv addOtherButtonWithTitle:@"返回" style:WLAlertActionStyleDefault];
+    //添加输入框
+    [alertv addAlertViewTextFieldWithStyle:WLAlertViewTextFieldStyleSecureTextInput
+              textFieldPlaceholderAtIndex0:@"请输入密码"
+              textFieldPlaceholderAtIndex1:nil];
+    //显示
     [alertv show];
 
 }
